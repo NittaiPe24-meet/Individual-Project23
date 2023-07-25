4359dc6b35461204534d3db30e2b13b0b1862383
NittaiPe24-meet <nittai.peri@gmail.com>
2023-07-25 17:46:30 +0300

    ssheesh

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 import random
+# import amogus
 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
@@ -14,7 +15,7 @@ config = {
   "messagingSenderId": "780923332394",
   "appId": "1:780923332394:web:a2b6ff385ad04b194aa053",
   "measurementId": "G-JEWN2T84LD",
-  "databaseURL":"https://netroulette0-default-rtdb.firebaseio.com/"
+  "databaseURL":"https://netroulette0-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
 firebase = pyrebase.initialize_app(config)
@@ -22,7 +23,12 @@ auth = firebase.auth()
 db = firebase.database()
 
 
-@app.route('/', methods=['GET', 'POST'])
+
+
+
+
+
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
     return render_template("signin.html")
 
@@ -32,16 +38,16 @@ def signup():
     error = ""
     if request.method == 'POST':
 
-       
+        email = request.form['email']
+        password = request.form['password']
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
             UID = login_session['user']['localId']
-            user = {"email":(request.form['email']), "password":(request.form['password'])};
+            user = {"email": email, "password":password};
             db.child("Users").child(UID).set(user)
-
-
-            return redirect(url_for('add_tweet'))
-        except:
+            return redirect(url_for('home'))
+        except Exception as e:
+            print(e)
             error = "Authentication failed"
     return render_template("signup.html")
 # Your code should be below
